7a7c83f6c95f595314b51a16504a8248584ee6a4
John Joo <johnjoo1@users.noreply.github.com>
2021-04-04 23:03:57 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 print("hi")
 import pandas as pd
-df = pd.read_csv("/valohai/inputsNeighborhood_zhvi_uc_sfr_sm_sa_mon.csv")
+df = pd.read_csv("/valohai/inputs/Neighborhood_zhvi_uc_sfr_sm_sa_mon.csv")
 df.head().to_csv("/valohai/outputs/writecsv.csv")
